7e7025755d99faa89f574649a69212097d37011b
M1LKTEA <3494199620@qq.com>
2023-11-30 22:55:39 +0800

    1

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask
+import escape
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return '<h1>Hello Totoro!</h1>'
+@app.route('/user/<name>')
+def user_page(name):
+    return f'User: {escape(name)}'
