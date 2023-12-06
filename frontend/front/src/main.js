af4da4f7b6e817ceff330fa0befca4c4911f5824
M1LKTEA <3494199620@qq.com>
2023-12-06 11:14:19 +0800

    实现在前端向后端发起请求

--- frontend/front/src/main.js
+++ frontend/front/src/main.js
@@ -2,8 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import VueAxios from 'vue-axios'
+import axios from 'axios'
+import request from './utils/request';
 
 Vue.config.productionTip = false
+Vue.use(ElementUI).use(VueAxios,axios);
+Vue.prototype.$request=request
 
 new Vue({
   router,
--- frontend/front/src/views/main.vue
+++ frontend/front/src/views/main.vue
@@ -1,5 +1,13 @@
 <template>
-    <h1>hello world!</h1>
+    <body>
+        <h1>hello world!</h1>
+        <el-card style="width: 1050px;margin: 0px auto;">
+            <el-table :data="FlowPacket" style="width: 1000px;margin: 0px auto;border: 1px solid rgb(0, 0, 0);">
+            <el-table-column label="Summary" prop="summary"></el-table-column>
+            </el-table>
+        </el-card>
+        <el-button type="primary" style="background:#505458 ;border:none ;margin-top: 40px;" @click="FlowCatch()">抓取</el-button>
+    </body>
 </template>
 
 <script>
@@ -7,7 +15,35 @@ export default{
     name:'main',
     data(){
         return{
-            FlowPacket:[]
+            FlowPacket:[],
+            testdata:[{
+            "summary": "Ether / PPPoE / PPP / IP / TCP 111.13.116.110:https > 10.108.26.90:65421 PA / Raw"
+        },
+        {
+            "summary": "Ether / PPPoE / PPP / IP / TCP 10.108.26.90:65421 > 111.13.116.110:https PA / Raw"
+        },
+        {
+            "summary": "Ether / PPPoE / PPP / IP / TCP 111.13.116.110:https > 10.108.26.90:65421 A / Padding"
+        },
+        {
+            "summary": "Ether / PPPoE / PPP / IP / UDP 10.108.26.90:58526 > 183.236.59.177:9999 / Raw"
+        },
+        {
+            "summary": "Ether / PPPoE / PPP / IP / TCP 10.108.26.90:63801 > 20.189.173.15:https FA"
+            }]
+        }
+    },
+    methods:{
+        FlowCatch(){
+            this.$request.get('/').then(res=>{
+                if(res.code=='200'){
+                    this.FlowPacket=res.data
+                    this.$message({
+                    message: res.msg,
+                    type: 'success',
+                });
+                }
+            })
         }
     }
 }
