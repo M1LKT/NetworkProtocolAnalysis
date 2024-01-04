1f7b429459ade62edbacb439bff8fb026e9907a4
M1LKTEA <3494199620@qq.com>
2024-01-04 13:22:52 +0800

    修改错误链接

--- backend/utils/StarFireAPI/AIAnalysisFlow.py
+++ backend/utils/StarFireAPI/AIAnalysisFlow.py
@@ -1,14 +1,23 @@
 import utils.StarFireAPI.SparkApi as SparkApi
+import json
+
+def load_config():
+    with open('utils\\StarFireAPI\\config.json', 'r') as f:
+        config = json.load(f)
+    return config
+
+config = load_config()
+
 #以下密钥信息从控制台获取
-appid = "807bb902"     #填写控制台中获取的 APPID 信息
-api_secret = "Yjk3ODAxM2QwYjE3NmU0YzU2OGE0NDI3"   #填写控制台中获取的 APISecret 信息
-api_key ="1eb2843d08f441b5409b6746bf964417"    #填写控制台中获取的 APIKey 信息
+appid = config['AppId']     #填写控制台中获取的 APPID 信息
+api_secret = config['APISecret']   #填写控制台中获取的 APISecret 信息
+api_key =config['APIKey']    #填写控制台中获取的 APIKey 信息
 
 #用于配置大模型版本，默认“general/generalv2”
-domain = "generalv3"   # v1.5版本
+domain = config['domain']   # v1.5版本
 # domain = "generalv2"    # v2.0版本
 #云端环境的服务地址
-Spark_url = "ws://spark-api.xf-yun.com/v3.1/chat"  # v1.5环境的地址
+Spark_url = config['Spark_url']  # v1.5环境的地址
 # Spark_url = "ws://spark-api.xf-yun.com/v2.1/chat"  # v2.0环境的地址
 
 
